5201dde3162004d101d6b417be11ea978340db8f
EFE TILU <pathoferebus@gmail.com>
2020-05-17 20:33:02 +0300

    Updates

---
+++ precache-manifest.8f237c9874ca6736927df0b683d01265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5259a01f79bacaaf5c3026fb5e981a8",
+    "url": "//basics/index.html"
+  },
+  {
+    "revision": "6969b51ce9013142d0ff",
+    "url": "//basics/static/css/main.bbba21d9.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "//basics/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//basics/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6969b51ce9013142d0ff",
+    "url": "//basics/static/js/main.8492448b.chunk.js"
+  },
+  {
+    "revision": "5b641efaa4e985c94b49",
+    "url": "//basics/static/js/runtime-main.a9b13cd1.js"
+  }
+]);
